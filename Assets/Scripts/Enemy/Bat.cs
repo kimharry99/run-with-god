9bf42bb597b47f71b70bcc4bf982ff24fa177fe3
FrozenThera <dongmin11004@korea.ac.kr>
2019-06-19 18:28:20 +0900

    dummymonster에서 state변경이라고 생각되는 부분 붙어넣기 함

--- Assets/Scripts/Enemy/Bat.cs
+++ Assets/Scripts/Enemy/Bat.cs
@@ -6,6 +6,20 @@ public class Bat : NormalEnemy
 {
 	protected override void InitEnemy()
 	{
-		throw new System.NotImplementedException();
-	}
+
+        State idle = new State();
+        idle.StateUpdate += Idle;
+
+        State move = new State();
+        idle.StateUpdate += Moving;
+
+        State attack = new State();
+        attack.StateUpdate += AttackMelee;
+
+        stateMachine.AddNewState("idle", idle);
+        stateMachine.AddNewState("move", move);
+        stateMachine.AddNewState("attack", attack);
+
+        stateMachine.Transtion("idle");
+    }
 }
