8442c5973e66a9cb6c2416bd12e75f00e926f9f7
18유정화 <rhyun9584@naver.com>
2019-07-10 22:36:59 +0900

    점프 신탁 수정, 대쉬 신탁 추가, 점프대쉬 신탁 0번 에셋화

---
+++ Assets/Scripts/Trust/DashCountTrust.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashCountTrust : Trust
+{
+    public int needDashCount;
+    private int dashCount;
+
+    public void UpDashCount()
+    {
+        dashCount++;
+        Debug.Log("Dash Count : " + dashCount);
+    }
+
+    public override string GetDescription()
+    {
+        string desc = "";
+        foreach (var substring in description.Split(' ', '\n'))
+        {
+            if (substring == "%need_dash")
+            {
+                desc += needDashCount;
+            }
+            else
+            {
+                desc += substring;
+            }
+            desc += " ";
+        }
+        return desc;
+    }
+
+    public override bool IsDone { get { return dashCount >= needDashCount; } }
+
+    public override void Init()
+    {
+        dashCount = 0;
+        PlayerController.inst.OnDash += UpDashCount;
+        PlayerController.inst.OnDash += delegate { InGameUIManager.inst.UpdateTrustUI(this); };
+    }
+
+    public override string TrustToText()
+    {
+        return Mathf.Min(dashCount, needDashCount) + " / " + needDashCount;
+    }
+
+}
