6c2ed48b0ca18e308a673232c0dfad7a3646f9be
18유정화 <rhyun9584@naver.com>
2019-07-17 22:48:47 +0900

    시야안에 n마리 몬스터 m초 신탁 중간저장

---
+++ Assets/Scripts/Trust/EnemyInViewTrust.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInViewTrust : Trust
+{
+    public float limitTime;
+    public int limitEnemycount;
+    private float inRangeTime;
+    private int enemyCount;
+    private bool isDone;
+
+    public override string GetDescription()
+    {
+        string desc = "";
+        foreach (var substring in description.Split(' ', '\n'))
+        {
+            if (substring == "%limit_time")
+            {
+                desc += limitTime;
+            }
+            else if (substring == "%limit_count")
+            {
+                desc += limitEnemycount;   
+            }
+            else
+            {
+                desc += substring;
+            }
+            desc += " ";
+        }
+        return desc;
+    }
+
+    public override bool IsDone { get { return isDone; } }
+
+    public override void Init()
+    {
+        isDone = true;
+        inRangeTime = 0f;
+
+
+    }
+
+    public override string TrustToText()
+    {
+        string time = "Time : " + Mathf.Min(inRangeTime, limitTime) + " / " + limitTime;
+        string count = "Count : " + Mathf.Min(enemyCount, limitEnemycount) + " / " + limitEnemycount;
+
+        return time + "\n" + count;
+    }
+}
