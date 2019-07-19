bb97ab15643b96fe62815037c94ffa2ffe25c0f8
19 김동민 <dongmin11004@korea.ac.kr>
2019-07-19 22:09:38 +0900

    이제 방패병의 방패가 체력을 가지게 되고, 부서지기도 함 / 방패병의 bloodeffect가 이상한 position에 가있는것을 (0,0,0)으로 수정

---
+++ Assets/Scripts/Enemy/Shield.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : Shielder
+{
+   protected override void InitEnemy()
+    {
+
+    }
+
+    protected override void OnDead()
+    {
+        Destroy(gameObject);
+    }
+}
