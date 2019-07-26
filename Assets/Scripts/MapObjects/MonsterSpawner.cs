ea57078bb67f1e401fc8dcee5f6452ccf5bd31d7
bacon8282 <654321oiuytr@gmail.com>
2019-07-27 00:34:03 +0900

    몬스터 스포너 프로토타입

---
+++ Assets/Scripts/MapObjects/MonsterSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    public GameObject monster;  //소환할 몬스터
+    public float term;          //주기(초)
+    public int range;
+
+    void Start()
+    {
+        monster.transform.position = transform.position;
+
+        if(term != 0)
+            InvokeRepeating("Spawn", term, term);
+    }
+
+    public void Spawn()
+    {
+        if(monster != null)
+            Instantiate(monster);
+    }
+
+    public void StopSpawn()
+    {
+        CancelInvoke("Spawn");
+    }
+
+    public void SetMonster(GameObject newMonster)
+    {
+        monster = newMonster;
+        newMonster.transform.position = transform.position;
+    }
+
+    /*IEnumerator SpawnRoutine()
+    {
+        Spawn();
+
+        yield return new WaitForSeconds(term * Time.deltaTime);
+    }*/
+}
