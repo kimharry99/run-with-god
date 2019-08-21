0ac7d8944ba97c8f4eb3f23e17206c2a4de725e2
18김동민 <kimharry9927@gmail.com>
2019-08-21 13:54:59 +0900

    박쥐 움직임에 Time.deltaTime추가

--- Assets/Scripts/Enemy/Bat.cs
+++ Assets/Scripts/Enemy/Bat.cs
@@ -6,52 +6,64 @@ public class Bat : NormalEnemy
 {
 	public override EnemyType Type { get { return EnemyType.BAT; } }
 
-    private int cnt = 1;
-    private int direction;
-    private float coordinate;
+    //private int cnt = 1;
+    private bool direction;
+    private float x;
+    private Vector3 initialPosition;
     public ParticleSystem dead;
 
 
     protected override void Start()
     {
         base.Start();
-        direction = Random.Range(0, 2);
-        coordinate = Random.value * Mathf.PI + Random.value*Mathf.PI;
-        transform.position = new Vector2(transform.position.x + coordinate * 0.2f, transform.position.y+0.2f * Mathf.Sin(coordinate));
     }
 
     protected override void InitEnemy()
 	{
         State dead = new State();
         State roamc = new State();
-        State roamcc = new State();
+        //State roamcc = new State();
 
         dead.StateUpdate += Dead;
         roamc.StateUpdate += RoamC;
-        roamcc.StateUpdate += RoamCC;
+        //roamcc.StateUpdate += RoamCC;
 
         stateMachine.AddNewState("roamc",roamc);
-        stateMachine.AddNewState("roamcc", roamcc);
+        //stateMachine.AddNewState("roamcc", roamcc);
 		stateMachine.AddNewState("dead", dead);
-
-        if (direction == 1)
+        /*
+        if (!direction)
+        {
+            stateMachine.Transition("roamc");
+            Flip();
+        }
+        else
         {
             stateMachine.Transition("roamcc");
             Flip();
         }
-        else stateMachine.Transition("roamc");
+        */
+        initialPosition = transform.position;
+        x = Random.Range(0f, 2f);
+        direction = x < 1f;
+        if (!direction)
+        {
+            Flip();
+        }
+        stateMachine.Transition("roamc");
+        
        
     }
     protected void RoamC()
     {
         if(Health <= 0)
             stateMachine.Transition("dead");
-        
-        if (coordinate < 2 * Mathf.PI)
+        /*
+        if (x < 2 * Mathf.PI)
         {
 
-            transform.position = new Vector2(transform.position.x + 0.01f, transform.position.y + (0.4f * Mathf.Sin(coordinate + 0.05f) - 0.4f * Mathf.Sin(coordinate)) / 2);
-            coordinate += 0.07f;
+            transform.position = new Vector2(transform.position.x + Time.deltaTime, transform.position.y + Mathf.Cos(x));
+            x += Time.deltaTime;
         }
         else
         {
@@ -60,25 +72,54 @@ public class Bat : NormalEnemy
                 Flip();
                 cnt--;
             }
-            transform.position = new Vector2(transform.position.x - 0.01f, transform.position.y + (0.4f * Mathf.Sin(coordinate + 0.05f) - 0.4f * Mathf.Sin(coordinate)) / 2);
-            coordinate += 0.07f;
-            if (coordinate > 4 * Mathf.PI)
+            transform.position = new Vector2(transform.position.x - 0.01f, transform.position.y + (0.4f * Mathf.Sin(x + 0.05f) - 0.4f * Mathf.Sin(x)) / 2);
+            x += Time.deltaTime;
+            if (x > 4 * Mathf.PI)
             {
                 Flip();
-                coordinate -= 4 * Mathf.PI;
+                x -= 4 * Mathf.PI;
                 cnt++;
             }
         }
+        */
+
+        if (x < 1)
+        {
+            if (!direction)
+            {
+                Flip();
+                direction = true;
+            }
+            transform.position = new Vector3(initialPosition.x + 0.8f*(x - 0.5f), initialPosition.y + 0.2f * Mathf.Sin(2 * (x - 0.5f) * Mathf.PI), 0);
+            x += Time.deltaTime / 2;
+        }
+        else
+        {
+            if (direction)
+            {
+                Flip();
+                direction = false;
+            }
+            transform.position = new Vector3(initialPosition.x - 0.8f*(x - 1.5f), initialPosition.y + 0.2f * Mathf.Sin(2 * (x-0.5f) * Mathf.PI), 0);
+            x += Time.deltaTime / 2;
+            if (x >= 2)
+            {
+                x -= 2;
+            }
+        }
+
     }
+    /*
     protected void RoamCC()
     {
+  
         if (Health <= 0)
             stateMachine.Transition("dead");
 
-        if (coordinate > -2 * Mathf.PI)
+        if (x > -2 * Mathf.PI)
         {
-            transform.position = new Vector3(transform.position.x + 0.01f, transform.position.y + (0.4f*Mathf.Sin(coordinate + 0.05f) - 0.4f*Mathf.Sin(coordinate)) / 2, transform.position.z);
-            coordinate -= 0.07f;
+            transform.position = new Vector3(transform.position.x + 0.01f, transform.position.y + (0.4f*Mathf.Sin(x + 0.05f) - 0.4f*Mathf.Sin(x)) / 2, transform.position.z);
+            x -= 0.07f;
         }
         else
         {
@@ -87,16 +128,42 @@ public class Bat : NormalEnemy
                 Flip();
                 cnt--;
             }
-            transform.position = new Vector3(transform.position.x - 0.01f, transform.position.y + (0.4f*Mathf.Sin(coordinate + 0.05f) - 0.4f*Mathf.Sin  (coordinate)) / 2, transform.position.z);
-            coordinate -= 0.07f;
-            if (coordinate < -4 * Mathf.PI)
+            transform.position = new Vector3(transform.position.x - 0.01f, transform.position.y + (0.4f*Mathf.Sin(x + 0.05f) - 0.4f*Mathf.Sin  (x)) / 2, transform.position.z);
+            x -= 0.07f;
+            if (x < -4 * Mathf.PI)
             {
                 Flip();
                 cnt++;
-                coordinate += 4 * Mathf.PI;
+                x += 4 * Mathf.PI;
+            }
+        }
+        if (x < 1)
+        {
+            if (!direction)
+            {
+                Flip();
+                direction = true;
+            }
+            transform.position = new Vector3(initialPosition.x + (x - 0.5f), initialPosition.y - 0.2f * Mathf.Sin(2 * (x - 0.5f) * Mathf.PI), 0);
+            x += Time.deltaTime / 2;
+        }
+        else
+        {
+            if (direction)
+            {
+                Flip();
+                direction = false;
+            }
+            transform.position = new Vector3(initialPosition.x - (x - 1.5f), initialPosition.y - 0.2f * Mathf.Sin(2 * (x - 0.5f) * Mathf.PI), 0);
+            x += Time.deltaTime / 2;
+            if (x >= 2)
+            {
+                x -= 2;
+                Debug.Log(transform.position);
             }
         }
     }
+    */
 
     protected void Dead()
     {
