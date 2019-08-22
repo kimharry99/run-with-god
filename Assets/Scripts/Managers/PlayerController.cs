88b50509923dcefbcfad27f9130967780ef8dd8b
18김동민 <kimharry9927@gmail.com>
2019-08-23 00:57:17 +0900

    체력을 3으로 만들어 주는 치트 커맨드 추가 (키보드 k)

--- Assets/Scripts/Managers/PlayerController.cs
+++ Assets/Scripts/Managers/PlayerController.cs
@@ -273,6 +273,7 @@ public class PlayerController : SingletonBehaviour<PlayerController>
 		State idle = new State();
 		idle.StateUpdate += PlayerMovementControl;
 		idle.StateUpdate += PlayerAttackControl;
+        idle.StateUpdate += Cheat;
 
 		State hit = new State();
 		hit.Enter += delegate
@@ -414,10 +415,10 @@ public class PlayerController : SingletonBehaviour<PlayerController>
 		StopAllCoroutines();
 		StartCoroutine(DeadRoutine());
 	}
-	#endregion
+    #endregion
 
-	#region Action Routine Enumerators
-	private IEnumerator GraceTimeRoutine()
+    #region Action Routine Enumerators
+    private IEnumerator GraceTimeRoutine()
 	{
 		Color oriColor = sr.color;
 		gameObject.layer = LayerMask.NameToLayer("Player Grace");
@@ -550,7 +551,7 @@ public class PlayerController : SingletonBehaviour<PlayerController>
 		yield return new WaitForSeconds(0.3f);
 		Physics2D.IgnoreCollision(col, other, false);
 	}
-	#endregion
+    #endregion
 
     #region Deprecated Functions
     /*
@@ -566,5 +567,17 @@ public class PlayerController : SingletonBehaviour<PlayerController>
 		}
 	}
     */
+    #endregion
+
+    //for Debug
+    #region Cheat Functions
+    private void Cheat()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Life = 3;
+            Debug.Log(Life);
+        }
+    }
     #endregion
 }
